dc039638d0576c1e08ffe9a57f447206706d44f8
wongan4 <53811564+wongan4@users.noreply.github.com>
2020-04-09 15:01:56 +0800

    [Andy] pass test case: return car when give a ticket

--- src/main/java/com/oocl/ParkingBoy.java
+++ src/main/java/com/oocl/ParkingBoy.java
@@ -1,8 +1,19 @@
 package com.oocl;
 
 public class ParkingBoy {
+    private ParkingLot parkingLot;
+
+    public ParkingBoy(ParkingLot parkingLot) {
+        this.parkingLot = parkingLot;
+    }
+
     public ParkingTicket park(Car car) {
         ParkingTicket parkingTicket = new ParkingTicket();
         return parkingTicket;
+    }
+
+    public Car fetch(ParkingTicket parkingticket) {
+        Car car = new Car();
+        return car;
     }
 }
--- src/test/java/com/oocl/ParkingLotTest.java
+++ src/test/java/com/oocl/ParkingLotTest.java
@@ -12,12 +12,19 @@ public class ParkingLotTest {
     @Before
     public void setup() {
         this.parkingLot = new ParkingLot();
-        this.parkingBoy = new ParkingBoy();
+        this.parkingBoy = new ParkingBoy(parkingLot);
     }
 
     @Test
     public void should_return_ticket_when_park_car() {
         ParkingTicket parkingticket = this.parkingBoy.park(new Car());
         assertNotNull(parkingticket);
+    }
+
+    @Test
+    public void should_return_car_when_give_ticket() {
+        ParkingTicket parkingticket = this.parkingBoy.park(new Car());
+        Car car = this.parkingBoy.fetch(parkingticket);
+        assertNotNull(car);
     }
 }
